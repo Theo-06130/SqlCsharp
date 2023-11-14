143a677345912a9a94d5c7002482c4c356c751ec
Theo-06130 <theo.cerkownik@ynov.com>
2023-11-14 17:18:31 +0100

    test

--- CsharpSQL/Form1.cs
+++ CsharpSQL/Form1.cs
@@ -16,7 +16,6 @@ namespace CsharpSQL
 {
     public partial class Form1 : Form
     {
-
         
         // Déplacez la déclaration de la chaîne de connexion en dehors de la méthode
         private string connectionString = "Server=176.31.132.185;User ID=biblivres;Password=ig%B-7K2*59WzOc_;Database=biblivres";
@@ -25,7 +24,7 @@ namespace CsharpSQL
             InitializeComponent();
         }
         
-    
+
         private void rechargerToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // Créez une instance de MySqlConnection en utilisant la chaîne de connexion
@@ -35,7 +34,7 @@ namespace CsharpSQL
                 {
                     // Ouvrez la connexion
                     connection.Open();
-                  //  MessageBox.Show("Connexion réussie");
+                    MessageBox.Show("Connexion réussie");
                 }
                 catch (MySqlException co)
                 {
@@ -48,179 +47,9 @@ namespace CsharpSQL
 
         private void homeToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
-        }
-
-        private void genresToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // Créez une instance de MySqlConnection en utilisant la chaîne de connexion
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                try
-                {
-                    // Ouvrez la connexion
-                    connection.Open();
-    
-                    // Commande SQL pour récupérer toutes les données de la table "nom_de_votre_table"
-                    string query = "SELECT * FROM Genre";
-
-                    // Créez une instance de MySqlCommand avec la commande SQL et la connexion
-                    using (MySqlCommand command = new MySqlCommand(query, connection))
-                    {
-                        // Créez un lecteur de données pour récupérer les résultats de la commande SQL
-                        using (MySqlDataReader reader = command.ExecuteReader())
-                        {
-                            // Effacez les éléments existants dans le ListBox
-                            listBox1.Items.Clear();
-
-                            // Parcourez les résultats et ajoutez-les au ListBox
-                            while (reader.Read())
-                            {
-                                // Ajoutez ici les colonnes spécifiques que vous souhaitez afficher
-                                // Par exemple, si vous avez une colonne nom, vous pouvez ajouter : reader["Nom"].ToString()
-                                listBox1.Items.Add(reader["Titre_Genre"].ToString());
-                            }
-                        }
-                    }
-
-                 //   MessageBox.Show("Données récupérées et affichées avec succès");
-                }
-                catch (MySqlException ex)
-                {
-                    MessageBox.Show($"Erreur : {ex.Message}");
-                    MessageBox.Show("Échec de la récupération des données");
-                }
-            }
-            
-        }
-
-        private void livresToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // Créez une instance de MySqlConnection en utilisant la chaîne de connexion
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                try
-                {
-                    // Ouvrez la connexion
-                    connection.Open();
-    
-                    // Commande SQL pour récupérer toutes les données de la table "nom_de_votre_table"
-                    string query = "SELECT * FROM Livres";
-
-                    // Créez une instance de MySqlCommand avec la commande SQL et la connexion
-                    using (MySqlCommand command = new MySqlCommand(query, connection))
-                    {
-                        // Créez un lecteur de données pour récupérer les résultats de la commande SQL
-                        using (MySqlDataReader reader = command.ExecuteReader())
-                        {
-                            // Effacez les éléments existants dans le ListBox
-                            listBox1.Items.Clear();
-
-                            // Parcourez les résultats et ajoutez-les au ListBox
-                            while (reader.Read())
-                            {
-                                // Ajoutez ici les colonnes spécifiques que vous souhaitez afficher
-                                // Par exemple, si vous avez une colonne nom, vous pouvez ajouter : reader["Nom"].ToString()
-                                listBox1.Items.Add(reader["Titre_Livre"].ToString());
-                            }
-                        }
-                    }
-
-                   // MessageBox.Show("Données récupérées et affichées avec succès");
-                }
-                catch (MySqlException ex)
-                {
-                    MessageBox.Show($"Erreur : {ex.Message}");
-                    MessageBox.Show("Échec de la récupération des données");
-                }
-            }
-            
-        }
-
-        private void auteurToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // Créez une instance de MySqlConnection en utilisant la chaîne de connexion
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                try
-                {
-                    // Ouvrez la connexion
-                    connection.Open();
-    
-                    // Commande SQL pour récupérer toutes les données de la table "nom_de_votre_table"
-                    string query = "SELECT * FROM Auteur";
-
-                    // Créez une instance de MySqlCommand avec la commande SQL et la connexion
-                    using (MySqlCommand command = new MySqlCommand(query, connection))
-                    {
-                        // Créez un lecteur de données pour récupérer les résultats de la commande SQL
-                        using (MySqlDataReader reader = command.ExecuteReader())
-                        {
-                            // Effacez les éléments existants dans le ListBox
-                            listBox1.Items.Clear();
-
-                            // Parcourez les résultats et ajoutez-les au ListBox
-                            while (reader.Read())
-                            {
-                                // Ajoutez ici les colonnes spécifiques que vous souhaitez afficher
-                                // Par exemple, si vous avez une colonne nom, vous pouvez ajouter : reader["Nom"].ToString()
-                                listBox1.Items.Add(reader["Nom"].ToString());
-                            }
-                        }
-                    }
-
-                    //MessageBox.Show("Données récupérées et affichées avec succès");
-                }
-                catch (MySqlException ex)
-                {
-                    MessageBox.Show($"Erreur : {ex.Message}");
-                    MessageBox.Show("Échec de la récupération des données");
-                }
-            }
-            
-        }
-
-        private void typesToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            // Créez une instance de MySqlConnection en utilisant la chaîne de connexion
-            using (MySqlConnection connection = new MySqlConnection(connectionString))
-            {
-                try
-                {
-                    // Ouvrez la connexion
-                    connection.Open();
-    
-                    // Commande SQL pour récupérer toutes les données de la table "nom_de_votre_table"
-                    string query = "SELECT * FROM Types";
-
-                    // Créez une instance de MySqlCommand avec la commande SQL et la connexion
-                    using (MySqlCommand command = new MySqlCommand(query, connection))
-                    {
-                        // Créez un lecteur de données pour récupérer les résultats de la commande SQL
-                        using (MySqlDataReader reader = command.ExecuteReader())
-                        {
-                            // Effacez les éléments existants dans le ListBox
-                            listBox1.Items.Clear();
-
-                            // Parcourez les résultats et ajoutez-les au ListBox
-                            while (reader.Read())
-                            {
-                                // Ajoutez ici les colonnes spécifiques que vous souhaitez afficher
-                                // Par exemple, si vous avez une colonne nom, vous pouvez ajouter : reader["Nom"].ToString()
-                                listBox1.Items.Add(reader["Types"].ToString());
-                            }
-                        }
-                    }
-
-                    //MessageBox.Show("Données récupérées et affichées avec succès");
-                }
-                catch (MySqlException ex)
-                {
-                    MessageBox.Show($"Erreur : {ex.Message}");
-                    MessageBox.Show("Échec de la récupération des données");
-                }
-            }
-            
+            FormTest formTest = new FormTest();
+            formTest.Show();
+            throw new System.NotImplementedException();
         }
     }
     }
