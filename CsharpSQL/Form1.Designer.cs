300930e4283290dbbd7335e4ba0eca9b4c025fa8
Theo-06130 <theo.cerkownik@ynov.com>
2023-11-15 22:17:47 +0100

    V2.1 fini avec implémentation des différentes colonnnes et leurs info

--- CsharpSQL/Form1.Designer.cs
+++ CsharpSQL/Form1.Designer.cs
@@ -33,20 +33,23 @@
             this.homeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.genresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.auteurToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.livresToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.typesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.rechargerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
             this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.homeToolStripMenuItem, this.genresToolStripMenuItem, this.auteurToolStripMenuItem, this.typesToolStripMenuItem, this.rechargerToolStripMenuItem });
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] { this.homeToolStripMenuItem, this.genresToolStripMenuItem, this.auteurToolStripMenuItem, this.livresToolStripMenuItem, this.typesToolStripMenuItem, this.rechargerToolStripMenuItem });
             this.menuStrip1.Location = new System.Drawing.Point(15, 0);
             this.menuStrip1.Margin = new System.Windows.Forms.Padding(0, 0, 0, 15);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(770, 28);
+            this.menuStrip1.Size = new System.Drawing.Size(1106, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -74,6 +77,14 @@
             this.auteurToolStripMenuItem.Text = "Auteur";
             this.auteurToolStripMenuItem.Click += new System.EventHandler(this.auteurToolStripMenuItem_Click);
             // 
+            // livresToolStripMenuItem
+            // 
+            this.livresToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.livresToolStripMenuItem.Name = "livresToolStripMenuItem";
+            this.livresToolStripMenuItem.Size = new System.Drawing.Size(58, 24);
+            this.livresToolStripMenuItem.Text = "Livres";
+            this.livresToolStripMenuItem.Click += new System.EventHandler(this.livresToolStripMenuItem_Click_1);
+            // 
             // typesToolStripMenuItem
             // 
             this.typesToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
@@ -92,15 +103,35 @@
             // 
             // listBox1
             // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
             this.listBox1.Location = new System.Drawing.Point(15, 28);
             this.listBox1.Margin = new System.Windows.Forms.Padding(15);
             this.listBox1.Name = "listBox1";
-            this.listBox1.ScrollAlwaysVisible = true;
-            this.listBox1.Size = new System.Drawing.Size(770, 407);
+            this.listBox1.Size = new System.Drawing.Size(400, 486);
             this.listBox1.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(415, 28);
+            this.listBox3.Margin = new System.Windows.Forms.Padding(15, 3, 3, 3);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(706, 486);
+            this.listBox3.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(721, 28);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(400, 486);
+            this.listBox2.TabIndex = 3;
             // 
             // Form1
             // 
@@ -108,7 +139,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1136, 529);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox3);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -120,6 +153,12 @@
             this.ResumeLayout(false);
             this.PerformLayout();
         }
+
+        private System.Windows.Forms.ToolStripMenuItem livresToolStripMenuItem;
+
+        private System.Windows.Forms.ListBox listBox2;
+
+        private System.Windows.Forms.ListBox listBox3;
 
         private System.Windows.Forms.ListBox listBox1;
 
